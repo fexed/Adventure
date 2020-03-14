af817ab1991a6210e649cdfbcdaf2fdc1a8da556
Federico Matteoni <fedex15@live.it>
2020-03-14 11:47:55 +0100

    Formatting

--- adventure.py
+++ adventure.py
@@ -7,13 +7,13 @@ gold = 3
 def innChoice(text):
     print(text)
     print("(You have " + str(gold) + " coins)")
-    choice = input()
+    choice = input("\t")
     return choice
 
 
-name = input("####\n\nAdventurer, welcome! How may I call you?\n")
+name = input("####\n\nAdventurer, welcome! How may I call you?\n\t")
 print("\n\nGreetings " + name + ", and welcome to my humble inn!")
 print("My name is Alf, how may I be of service?")
 print("We have ale, pork, rooms and quests!")
 choice = innChoice("Ale\t1 coin\nPork\t1 coin\nRoom\t2 coins\nQuest")
-print("Oh, " + choice + "! Good!")
+print("\n\nOh, " + choice + "! Good!")
