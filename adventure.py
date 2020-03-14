86f098423ad3c00e1db273c89b17f623d52fae41
Federico Matteoni <fedex15@live.it>
2020-03-14 16:53:19 +0100

    Update adventure.py

--- adventure.py
+++ adventure.py
@@ -1,16 +1,24 @@
+import time
+
 # CHARACTER
 name = ""
-level = 1
+level = 0
 gold = 3
 
 def innChoice(text):
     print(text)
-    print("(You have " + str(gold) + " coins)")
+    print("\tCoins: " + str(gold))
     choice = input("\t")
     return choice.lower()
 
+print("#### Adventure under the stars ####\n\n\n\n")
+print("You find yourself inside a small inn, sitting at the counter and waiting for the bartender.")
+print("It's night outside, and there's quite a few people inside the inn.")
+print("The bartender approaches you as he finishes to serve another customer.")
+name = input("\"Adventurer, welcome! How may I call you?\"\n\t")
+while (name == ""):
+    name = input("\"Please, dear adventurer, perhaps a made up name?\"\n\t")
 
-name = input("####\n\n\"Adventurer, welcome! How may I call you?\"\n\t")
 print("\n\n\"Greetings " + name + ", and welcome to my humble inn!\"")
 print("\"My name is Alf, how may I be of service?\"")
 print("\"We have ale, pork, rooms and quests!\"")
@@ -26,6 +34,7 @@ while (choice != "quest"):
             print("\"Drink up " + name + "!\"")
             print("You give 1 coin to Alf")
             gold -= 1
+            print("The drink is strong and refreshing.")
         else:
             print("You do not have enough coins")
     elif (choice == "pork" or choice == "food"):
@@ -34,6 +43,7 @@ while (choice != "quest"):
             print("\"Eat merrily " + name + "!\"")
             print("You give 1 coin to Alf")
             gold -= 1
+            print("The food is delicious and filling.")
         else:
             print("You do not have enough coins")
     elif (choice == "room" or choice == "sleep"):
@@ -44,6 +54,11 @@ while (choice != "quest"):
             print("You give 2 coins to Alf")
             print("\"See you tomorrow!\"")
             gold -= 2
+            time.sleep(5)
+            print("\n\nThe next morning, you return downstairs.")
+            print("A fresh air fills up the entrance of the inn, brightly lit by the morning sun.")
+            print("Alf greets you, while he's behind the counter polishing some cups.")
+            print("\"Good morning, " + name + "! I hope you slept well!\"")
         else:
             print("You do not have enough coins")
 #do-while end
