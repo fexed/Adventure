9df730580bf19fc222f817db6852bcf1fb0c27da
Federico Matteoni <fedex15@live.it>
2020-03-15 01:50:18 +0100

    Working

--- adventure.py
+++ adventure.py
@@ -2,10 +2,10 @@ import time
 import curses
 
 # SYSTEM
-winWidth = 50
+winWidth = 60
 
 # CHARACTER
-name = "testName"
+name = ""
 level = 0
 gold = 3
 
@@ -14,6 +14,11 @@ def waitForKey(window):
 	curses.cbreak()
 	char = window.getch()
 	return char
+
+def input(window):
+    curses.echo()
+    txt = window.getch()
+    return txt
 
 def txtWindowBuild(window):
 	window.addstr(15, 1, "+" + ("-" * winWidth) + "+")
@@ -37,18 +42,25 @@ def txtShow(window, text):
 
 
 def charWindow(window):
-	window.addstr(15, 53, "+" + ("-" * 15) + "+")
+	window.addstr(15, winWidth+3, "+" + ("-" * 15) + "+")
 	ln = len(name)
-	window.addstr(16, 53, "|" + name + (" " * (15-ln)) + "|")
+	window.addstr(16, winWidth+3, "|" + name + (" " * (15-ln)) + "|")
 	ln = len("LV " + str(level))
-	window.addstr(17, 53, "|" + "LV " + str(level) + (" " * (15-ln)) + "|")
+	window.addstr(17, winWidth+3, "|" + "LV " + str(level) + (" " * (15-ln)) + "|")
 	ln = len("Coins " + str(gold))
-	window.addstr(18, 53, "|" + "Coins " + str(gold) + (" " * (15-ln)) + "|") 
-	window.addstr(19, 53, "+" + ("-" * 15) + "+")
+	window.addstr(18, winWidth+3, "|" + "Coins " + str(gold) + (" " * (15-ln)) + "|") 
+	window.addstr(19, winWidth+3, "+" + ("-" * 15) + "+")
 	window.refresh()
 
 def main(window):
-	txtShow(window, "Hello adventurer this is a really long text indeed perhaps a bit longer indeed indeed...")
+    txtShow(window, "Hello adventurer! This is a very small adventure, an experiment if you may... you will explore a very simple dungeon and try to retrieve some gold hidden in it!")
+    txtWindowBuild(window)
+    txtShow(window, "Would you like to go on an adventure?")
+    txtWindowBuild(window)
+    txtShow(window, "Let's begin")
+    txtWindowBuild(window)
+    txtShow(window, "What's your name?")
+    name = input(window)
 
 window = curses.initscr()
 curses.halfdelay(5)
